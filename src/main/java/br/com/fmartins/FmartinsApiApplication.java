5da6af32cf9f308caf76a263ab76146e4851984d
Mario <mario.martins@sankhya.com.br>
2020-08-04 20:03:27 -0300

    estruturação

---
+++ src/main/java/br/com/fmartins/FmartinsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmartins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmartinsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmartinsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fmartins/FmartinsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmartins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmartinsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

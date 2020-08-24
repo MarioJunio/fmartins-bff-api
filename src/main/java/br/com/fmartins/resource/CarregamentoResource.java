514598d37d2a42cd522f7029e68c4595ff90a1d8
Mario <mario.martins@sankhya.com.br>
2020-08-24 01:22:52 -0300

    ajuste na recurso de carregamentos nao finalizados

--- src/main/java/br/com/fmartins/resource/CarregamentoResource.java
+++ src/main/java/br/com/fmartins/resource/CarregamentoResource.java
@@ -4,12 +4,15 @@ import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fmartins.resource.dto.request.FinalizarCarregamentoDTO;
@@ -29,16 +32,17 @@ public class CarregamentoResource {
 	private final CarregamentoService carregamentoService;
 
 	@GetMapping("/{numCarregamento}")
-	public ResponseEntity<CarregamentoDTO> pesquisar(@PathVariable BigInteger numCarregamento) {
+	public ResponseEntity<CarregamentoDTO> pesquisar(@PathVariable BigInteger numCarregamento)
+			throws InterruptedException {
 
 		Optional<CarregamentoDTO> opCarregamento = carregamentoService.pesquisar(numCarregamento);
 
 		return opCarregamento.isPresent() ? ResponseEntity.ok(opCarregamento.get()) : ResponseEntity.notFound().build();
 	}
 
-	@GetMapping("/agrupados-nao-finalizados/{matricula}")
+	@GetMapping("/agrupados-nao-finalizados")
 	public ResponseEntity<List<CarregamentoAgrupadoNaoFinalizadoDTO>> pesquisarCargasNaoFinalizadasPorUsuario(
-			@PathVariable BigInteger matricula) {
+			@RequestParam BigInteger matricula) {
 
 		log.info("/agrupados-nao-finalizados/{}", matricula);
 
@@ -49,15 +53,17 @@ public class CarregamentoResource {
 				: ResponseEntity.notFound().build();
 	}
 
+	@ResponseStatus(HttpStatus.OK)
 	@PutMapping("/iniciar/{numCarregamento}")
 	public void iniciarCarregamento(@PathVariable BigInteger numCarregamento,
-			@RequestBody IniciarCarregamentoDTO payload) {
+			@RequestBody IniciarCarregamentoDTO payload) throws InterruptedException {
 		log.info("/iniciar/{}", numCarregamento);
 		log.info("{}", payload);
 
 		carregamentoService.iniciar(numCarregamento, payload);
 	}
 
+	@ResponseStatus(HttpStatus.OK)
 	@PutMapping("/finalizar")
 	public void finalizarCarga(@RequestBody FinalizarCarregamentoDTO payload) {
 		log.info("/finalizar");
